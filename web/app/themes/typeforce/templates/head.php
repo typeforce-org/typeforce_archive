4b74913c2f0a7afa402bcc739f513ee71d0e8ec5
Bryant Smith <bryant.robert.smith@gmail.com>
2016-05-09 11:01:18 -0500

    add global color and typography styles

--- web/app/themes/typeforce/templates/head.php
+++ web/app/themes/typeforce/templates/head.php
@@ -3,4 +3,5 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+  <link href='http://fonts.googleapis.com/css?family=Karla:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
 </head>
