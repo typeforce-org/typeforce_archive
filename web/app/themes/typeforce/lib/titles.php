44f2d6f4721de185f73b3c486d4a22d1c6e2cde7
Bryant Smith <bryant.robert.smith@gmail.com>
2016-05-25 11:26:09 -0500

    Icon mouse-overs; Nate’s design fixes

--- web/app/themes/typeforce/lib/titles.php
+++ web/app/themes/typeforce/lib/titles.php
@@ -17,7 +17,7 @@ function title() {
   } elseif (is_archive()) {
     return get_the_archive_title();
   } elseif (is_search()) {
-    return sprintf(__('Search results for "%s":', 'sage'), get_search_query());
+    return sprintf(__('Search results for "%s"', 'sage'), get_search_query());
   } elseif (is_404()) {
     return __('Not Found', 'sage');
   } else {
