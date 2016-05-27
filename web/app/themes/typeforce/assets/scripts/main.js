cb6c43fd6610826d1eb7664603ed80d0251e53c0
Bryant Smith <bryant.robert.smith@gmail.com>
2016-05-27 13:58:45 -0500

    refactor navbar—don’t position things with pixels, you fool.

--- web/app/themes/typeforce/assets/scripts/main.js
+++ web/app/themes/typeforce/assets/scripts/main.js
@@ -146,7 +146,7 @@ var FBSage = (function($) {
         _hideNav();
       });
     $('<svg class="menu-toggle menu-toggle-open icon-hamburger" role="img"><use xlink:href="#icon-hamburger"></use></svg>')
-      .prependTo('.site-header')
+      .prependTo('.site-header .wrapper')
       .on('click', function(e) {
         _showNav();
       });
@@ -263,15 +263,15 @@ var FBSage = (function($) {
   }
 
   function _fixHeaderOnScroll() {
-    // $(window).scroll(function() {
-    //   var wintop = $(window).scrollTop();
-    //   var distance = 30; //distance = breakpoint_medium ? 50 : 30;
-    //   if (wintop > distance) {
-    //     $('.home .site-header').addClass('scrolled');
-    //   } else {
-    //     $('.home .site-header').removeClass('scrolled');
-    //   }
-    // });
+    $(window).scroll(function() {
+      var wintop = $(window).scrollTop();
+      var distance = 30; //distance = breakpoint_medium ? 50 : 30;
+      if (wintop > distance) {
+        $('.home .site-header').addClass('scrolled');
+      } else {
+        $('.home .site-header').removeClass('scrolled');
+      }
+    });
   }
 
 
--- web/app/themes/typeforce/templates/header.php
+++ web/app/themes/typeforce/templates/header.php
@@ -3,21 +3,21 @@ $title = '<a href="'.get_home_url().'">Typeforce</a>';
 ?>
 <header class="site-header" role="banner">
 
-  
-  <h1 class="title"><?= $title ?></h1>
+  <div class="wrapper">
+    <h1 class="title"><?= $title ?></h1>
 
-  <div class="search">
-    <?php get_search_form(); ?>
+    <div class="search">
+      <?php get_search_form(); ?>
+    </div>
+    <button class="search-toggle"></button>
+    <nav class="site-nav" role="navigation">
+
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+      endif;
+      ?>
+    </nav>
   </div>
-  <button class="search-toggle"></button>
-  <nav class="site-nav" role="navigation">
-
-    <?php
-    if (has_nav_menu('primary_navigation')) :
-      wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-    endif;
-    ?>
-  </nav>
-
 
 </header>
