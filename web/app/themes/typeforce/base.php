8de46e255264f96f3c32ea60ab0dafe47d7a1623
Bryant Smith <bryant.robert.smith@gmail.com>
2016-05-28 15:53:54 -0500

    try -ms- prefix

--- web/app/themes/typeforce/base.php
+++ web/app/themes/typeforce/base.php
@@ -37,5 +37,8 @@ use Roots\Sage\Wrapper;
       get_template_part('templates/footer');
       wp_footer();
     ?>
+    <script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.12.5.js'><\/script>".replace("HOST", location.hostname));
+//]]></script>
   </body>
 </html>
