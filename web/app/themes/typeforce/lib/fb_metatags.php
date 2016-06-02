c5ac26b3b5932fd9da2a59619652963a4e380665
Bryant Smith <bryant.robert.smith@gmail.com>
2016-06-02 02:33:02 -0500

    remove def image from exhibition

--- web/app/themes/typeforce/lib/fb_metatags.php
+++ web/app/themes/typeforce/lib/fb_metatags.php
@@ -89,12 +89,14 @@ function build_tags() {
       $metatag_images = array_merge($metatag_images, find_images()); // Returns an array already, so merge into existing
     }
   }
-  // Get default metatag image from site_options
-  $default_metatag_image = wp_get_attachment_image_src(\Firebelly\SiteOptions\get_option('default_metatag_image_id'), 'large')[0];
-  // Add the fallback image to the images array
-  if ($default_metatag_image) {
-    $metatag_images[] = $default_metatag_image;
-    // $metatag_images = array_reverse($metatag_images);
+  if(!is_tax('exhibition') && !is_single()){
+    // Get default metatag image from site_options
+    $default_metatag_image = wp_get_attachment_image_src(\Firebelly\SiteOptions\get_option('default_metatag_image_id'), 'large')[0];
+    // Add the fallback image to the images array
+    if ($default_metatag_image) {
+      $metatag_images[] = $default_metatag_image;
+      // $metatag_images = array_reverse($metatag_images);
+    }
   }
   // Make sure there were images passed as an array and loop through/output each
   if (!empty($metatag_images)) {
