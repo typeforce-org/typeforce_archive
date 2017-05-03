2187cfe86c5f9576db187b683750fdc31dd17c79
Bryant Smith <bryant.robert.smith@gmail.com>
2017-05-03 15:36:50 -0500

    Add support for special characters in Blažo Čalovíć's name in Karla font

--- web/app/themes/typeforce/templates/head.php
+++ web/app/themes/typeforce/templates/head.php
@@ -3,7 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
-  <link href='http://fonts.googleapis.com/css?family=Karla:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+  <link href="https://fonts.googleapis.com/css?family=Karla:400,400i,700,700i&amp;subset=latin-ext" rel="stylesheet">
   <link rel="icon" type="image/png" href="<?= Roots\Sage\Assets\asset_path('images/favicon.png'); ?>">
   <script src="<?= Roots\Sage\Assets\asset_path('scripts/modernizr.custom.js'); ?>"></script>
   <svg class="filter-defs">
