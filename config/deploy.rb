1b88ebb28c7ddb2f8a799082c0acad63989cd9d7
Bryant Smith <bryant.robert.smith@gmail.com>
2017-06-05 16:18:00 -0500

    Add web/php.ini to linked files for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :log_level, :info
 
 # Apache users with .htaccess files:
 # it needs to be added to linked_files so it persists across deploys:
-set :linked_files, fetch(:linked_files, []).push('.env', 'web/.htaccess')
+set :linked_files, fetch(:linked_files, []).push('.env', 'web/.htaccess', 'web/php.ini')
 set :linked_dirs, fetch(:linked_dirs, []).push('web/app/uploads')
 
 namespace :deploy do
