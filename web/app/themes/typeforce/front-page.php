291933f9cb102a938f47e8a2a7b35164b9868384
Nate Beaty <nate@clixel.com>
2019-01-17 11:45:24 -0600

    temporary cowboy homepage tweaks for TFX

--- web/app/themes/typeforce/front-page.php
+++ web/app/themes/typeforce/front-page.php
@@ -13,13 +13,17 @@ $thumb_id = get_post_thumbnail_id($post->ID);
 $thumb_url = wp_get_attachment_image_src($thumb_id, 'slide' )[0];
 ?>
 
-
-  <div class="intro-content bigclicky" style="background-image: url('<?= $thumb_url ?>')">
-    <div class="headline">
-      <div class="wrap">
-        <?= $headline ?>
-      </div>
+<!-- cowboy wildstyle -->
+<div class="intro-content bigclicky" style="height:auto;padding:0">
+  <div class="video">
+    <iframe src="https://player.vimeo.com/video/311559805?background=1;autoplay=1&autopause=0&loop=1&title=0&byline=0&portrait=0" width="960" height="540" frameborder="0"></iframe>
+  </div>
+  <div class="headline" style="height:100%;">
+    <div class="wrap">
+      <?= $headline ?>
     </div>
   </div>
+</div>
+<!-- repo man -->
 
 <?= $exhibits ?>
